f218a143a5b8c3c5d310aa2c6591145840f94930
Scott Keller <skeller@eloquii.com>
2018-06-15 13:48:33 -0400

    Fixed class name of test_main module. Added ability to read file in the main module

--- wordsearch-pkg/wordsearch/core/main.py
+++ wordsearch-pkg/wordsearch/core/main.py
@@ -2,6 +2,22 @@
 MODULE: core.main
 DESCRIPTION: Creates and solves a wordsearch game given the words and grid from an input file
 """
+import codecs
+import os
 
 def solve_wordsearch(path):
     pass
+
+def read_file(path):
+    """Reads a file from a given path"""
+    return codecs.open(filename=resolve_path(path), mode='r', encoding='utf-8')
+
+def resolve_path(path):
+    """Resolves relative paths and returns the absolute path"""
+    if path.startswith('~'):
+        path = os.path.expanduser(path)
+
+    elif os.path.isabs(path) is False:
+        path = os.path.join(os.path.abspath(os.getcwd()), path)
+
+    return os.path.realpath(path)
--- wordsearch-pkg/wordsearch/tests/test_wordsearch.py
+++ wordsearch-pkg/wordsearch/tests/test_wordsearch.py
@@ -4,12 +4,22 @@ DESCRIPTION: Runs unit tests on the main module
 """
 
 import unittest
+import os
 from ..core import main
 
-class TestWordSearch(unittest.TestCase):
+
+
+# Constants
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+class TestMain(unittest.TestCase):
     """Unit tests for the min module"""
 
     def test_solve_wordsearch_exists(self):
         """Test the solve_wordsearch function exists"""
         self.assertIsNotNone(main.solve_wordsearch)
         self.assertTrue(callable(main.solve_wordsearch))
+
+    def test_read_file(self):
+        """Tests reading a file"""
+        self.assertIsNotNone(main.read_file(os.path.join(DIR_PATH, 'test_input.txt')))
