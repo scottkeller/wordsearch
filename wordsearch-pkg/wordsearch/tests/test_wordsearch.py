893eb5a768136d698467e8614d9c0c5d8c49082a
Scott Keller <skeller@eloquii.com>
2018-06-17 18:19:00 -0400

    Added additional tests for all directions of wordsearch.search method

--- wordsearch-pkg/wordsearch/tests/test_wordsearch.py
+++ wordsearch-pkg/wordsearch/tests/test_wordsearch.py
@@ -43,6 +43,36 @@ class TestWordSearch(unittest.TestCase):
         self.assertEqual(my_wordsearch.search('word'), [(0,0), (1,0), (2,0), (3,0)])
 
     def test_search_horizontal_left(self):
-        """Test finding a word horizontally to the right """
+        """Test finding a word horizontally to the left """
         my_wordsearch = self.wordsearch(['w,o,r,l\n', 'x,x,x,x\n', 'd,r,o,w'])
         self.assertEqual(my_wordsearch.search('word'), [(3,2), (2,2), (1,2), (0,2)])
+
+    def test_vertical_down(self):
+        """Test finding a word vertically down"""
+        my_wordsearch = self.wordsearch(['w,x,x,x\n', 'o,x,x,x\n', 'r,x,x,x\n', 'd,x,x,x'])
+        self.assertEqual(my_wordsearch.search('word'), [(0,0), (0,1), (0,2), (0,3)])
+
+    def test_vertical_up(self):
+        """Test finding a word vertically up"""
+        my_wordsearch = self.wordsearch(['d,x,x,x\n', 'r,x,x,x\n', 'o,x,x,x\n', 'w,x,x,x'])
+        self.assertEqual(my_wordsearch.search('word'), [(0,3), (0,2), (0,1), (0,0)])
+
+    def test_diagonal_ascending_right(self):
+        """Test finding a word diagonally ascending to the right"""
+        my_wordsearch = self.wordsearch(['x,x,x,d\n', 'x,x,r,x\n', 'x,o,x,x\n', 'w,x,x,x'])
+        self.assertEqual(my_wordsearch.search('word'), [(0,3), (1,2), (2,1), (3,0)])
+
+    def test_diagonal_ascending_left(self):
+        """Test finding a word diagonally ascending to the left"""
+        my_wordsearch = self.wordsearch(['d,x,x,x\n', 'x,r,x,x\n', 'x,x,o,x\n', 'x,x,x,w'])
+        self.assertEqual(my_wordsearch.search('word'), [(3,3), (2,2), (1,1), (0,0)])
+
+    def test_diagonal_descending_right(self):
+        """Test finding a word diagonally descending to the right"""
+        my_wordsearch = self.wordsearch(['w,x,x,x\n', 'x,o,x,x\n', 'x,x,r,x\n', 'x,x,x,d'])
+        self.assertEqual(my_wordsearch.search('word'), [(0,0), (1,1), (2,2), (3,3)])
+
+    def test_diagonal_descending_left(self):
+        """Test finding a word diagonally descending to the left"""
+        my_wordsearch = self.wordsearch(['x,x,x,w\n', 'x,x,o,x\n', 'x,r,x,x\n', 'd,x,x,x'])
+        self.assertEqual(my_wordsearch.search('word'), [(3,0), (2,1), (1,2), (0,3)])
